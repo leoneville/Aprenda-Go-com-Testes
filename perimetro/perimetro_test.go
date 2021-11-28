139559ea45b9d9f503d38c95fe1f9f294a47987f
leoneville <leoneville_@hotmail.com>
2021-11-28 00:06:16 -0300

    Adicionando struct anonima e Table driven tests

--- perimetro/perimetro_test.go
+++ perimetro/perimetro_test.go
@@ -15,22 +15,18 @@ func TestPerimetro(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	verificaArea := func(t *testing.T, forma Forma, esperado float64) {
-		t.Helper()
-		resultado := forma.Area()
-
-		if resultado != esperado {
-			t.Errorf("resultado: '%.2f', esperado: '%.2f'", resultado, esperado)
-		}
+	testesArea := []struct { // struct anônima
+		forma    Forma
+		esperado float64
+	}{
+		{Retangulo{12, 6}, 72.0},
+		{Circulo{10}, 314.1592653589793},
 	}
 
-	t.Run("retângulos", func(t *testing.T) {
-		retangulo := Retangulo{12.0, 6.0}
-		verificaArea(t, retangulo, 72.0)
-	})
-
-	t.Run("circulos", func(t *testing.T) {
-		circulo := Circulo{10}
-		verificaArea(t, circulo, 314.1592653589793)
-	})
+	for _, tableTests := range testesArea {
+		resultado := tableTests.forma.Area()
+		if resultado != tableTests.esperado {
+			t.Errorf("resultado: '%.2f', esperado: '%.2f'", resultado, tableTests.esperado)
+		}
+	}
 }
