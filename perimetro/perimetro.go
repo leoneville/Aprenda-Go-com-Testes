09d2a5a7b7eaf6e3ff1ef03528b181e2c65d313b
leoneville <leoneville_@hotmail.com>
2021-11-27 23:11:27 -0300

    Refatorando e adicionando structs

---
+++ perimetro/perimetro.go
@@ -0,0 +1,14 @@
+package perimetro
+
+type Retangulo struct {
+	Largura float64
+	Altura  float64
+}
+
+func Perimetro(retangulo Retangulo) float64 {
+	return 2 * (retangulo.Largura + retangulo.Altura)
+}
+
+func Area(retangulo Retangulo) float64 {
+	return retangulo.Largura * retangulo.Altura
+}
