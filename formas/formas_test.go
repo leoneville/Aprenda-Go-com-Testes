d7090de85d07e4b8c09305e7faf7b06823faf1c1
leoneville <leoneville_@hotmail.com>
2021-11-28 00:14:23 -0300

    Adicionando forma triangulo

---
+++ formas/formas_test.go
@@ -0,0 +1,33 @@
+package formas
+
+import (
+	"testing"
+)
+
+func TestPerimetro(t *testing.T) {
+	retangulo := Retangulo{10.0, 10.0}
+	resultado := Perimetro(retangulo)
+	esperado := 40.0
+
+	if resultado != esperado {
+		t.Errorf("resultado: '%.2f', esperado: '%.2f'", resultado, esperado)
+	}
+}
+
+func TestArea(t *testing.T) {
+	testesArea := []struct { // struct anônima
+		forma    Forma
+		esperado float64
+	}{
+		{Retangulo{12, 6}, 72.0},
+		{Circulo{10}, 314.1592653589793},
+		{Triangulo{12, 6}, 36.0},
+	}
+
+	for _, tableTests := range testesArea {
+		resultado := tableTests.forma.Area()
+		if resultado != tableTests.esperado {
+			t.Errorf("resultado: '%.2f', esperado: '%.2f'", resultado, tableTests.esperado)
+		}
+	}
+}
